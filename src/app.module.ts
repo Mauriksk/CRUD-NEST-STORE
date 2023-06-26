574d5712c31c44a3d32d47fa3e7982daa39889c8
Mauricio Torres <otarksk@gmail.com>
2023-06-26 11:25:26 -0300

    created store module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { StoreModule } from './store/store.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [StoreModule],
 })
 export class AppModule {}
